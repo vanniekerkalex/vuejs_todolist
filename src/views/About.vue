85a315b981eee01d848f4b4745370bb996b9a815
Alex van Niekerk <alex.vanniekerk@medbrief.co.uk>
2019-09-19 15:43:46 +0200

    Completed Vue.js TodoList tutorial

---
+++ src/views/About.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="about">
+    <h1>About</h1>
+    <p>The volume in question contained, besides the ordinary letter- press, 
+      several poems by William Strachey and an autograph inscription written 
+      in the most wonderfully neat and clear handwriting-a standard in handwriting 
+      to which no member of the family before or since has ever attained.</p>
+  </div>
+</template>
